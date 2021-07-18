fe99ed301393b294e627c3168ed484485b845a8b
sakurafisch <sakurafisch@gmail.com>
2021-07-18 09:38:30 +0800

    Implement getStaticPaths and getStaticProps

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+        </Layout>
+    );
+}
+
+
+// 返回 id
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+
+// 接收 id 参数，并根据 id 返回对应的 post
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    };
+}
